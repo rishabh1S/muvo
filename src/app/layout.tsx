4db2bb8fa65d5155af196a2d5900dd0c33f979c8
Rishabh Singh <rishabh15112001@gmail.com>
2024-02-07 01:39:54 +0530

    Enable speed insights from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import AuthContext from "./context/AuthContext";
 import ToasterContext from "./context/ToasterContext";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Muvo",
@@ -21,6 +22,7 @@ export default function RootLayout({
           <ToasterContext />
           {children}
           <Analytics />
+          <SpeedInsights />
         </AuthContext>
       </body>
     </html>
